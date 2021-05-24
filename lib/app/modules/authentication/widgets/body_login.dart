e14d760ed3e6ecbdbb5bb9b6cbb5b1343b1a7e93
Alef Emannuel <alefemannuelifrn@gmail.com>
2021-05-24 11:08:24 -0300

    overflowed pixels

--- lib/app/modules/authentication/widgets/body_login.dart
+++ lib/app/modules/authentication/widgets/body_login.dart
@@ -15,59 +15,61 @@ class BodyLogin extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SvgPicture.asset(
-            "assets/icons/login.svg",
-            height: size.height * 0.35,
-          ),
-          RoundedInputField(
-            hintText: "Usuário",
-            onChanged: (value) {},
-          ),
-          RoundedPasswordField(
-            onChanged: (value) {},
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 15),
-          ),
-          Container(
-            width: size.width * 0.8,
-            height: 50,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text("LOGIN"),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(kPrimaryColor),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(29.0),
-                    ),
-                  )),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SvgPicture.asset(
+              "assets/icons/login.svg",
+              height: size.height * 0.35,
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 15),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Ainda não possui uma conta? ",
-                style: TextStyle(color: kPrimaryColor),
+            RoundedInputField(
+              hintText: "Usuário",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15),
+            ),
+            Container(
+              width: size.width * 0.8,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("LOGIN"),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(kPrimaryColor),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(29.0),
+                      ),
+                    )),
               ),
-              GestureDetector(
-                onTap: () {},
-                child: Text(
-                  "Cadastre-se",
-                  style: TextStyle(
-                      color: kPrimaryColor, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Ainda não possui uma conta? ",
+                  style: TextStyle(color: kPrimaryColor),
                 ),
-              ),
-            ],
-          ),
-        ],
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "Cadastre-se",
+                    style: TextStyle(
+                        color: kPrimaryColor, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
