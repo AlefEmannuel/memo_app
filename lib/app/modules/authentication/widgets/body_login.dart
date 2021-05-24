02545bb394695fd3fffc37ae30a02f5942f7c0c6
Alef Emannuel <alefemannuelifrn@gmail.com>
2021-05-24 10:50:35 -0300

    cadastre-se

--- lib/app/modules/authentication/widgets/body_login.dart
+++ lib/app/modules/authentication/widgets/body_login.dart
@@ -23,14 +23,18 @@ class BodyLogin extends StatelessWidget {
             height: size.height * 0.35,
           ),
           RoundedInputField(
-            hintText: "Login",
+            hintText: "Usuário",
             onChanged: (value) {},
           ),
           RoundedPasswordField(
             onChanged: (value) {},
           ),
+          Padding(
+            padding: EdgeInsets.only(top: 15),
+          ),
           Container(
             width: size.width * 0.8,
+            height: 50,
             child: ElevatedButton(
               onPressed: () {},
               child: Text("LOGIN"),
@@ -38,10 +42,30 @@ class BodyLogin extends StatelessWidget {
                   backgroundColor: MaterialStateProperty.all(kPrimaryColor),
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                     RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
+                      borderRadius: BorderRadius.circular(29.0),
                     ),
                   )),
             ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 15),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Ainda não possui uma conta? ",
+                style: TextStyle(color: kPrimaryColor),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  "Cadastre-se",
+                  style: TextStyle(
+                      color: kPrimaryColor, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
           ),
         ],
       ),
